73f5f4c15f971916492c9d76d4a3d8fdab3a5fda
Dammy Ola <dammyolapaix@gmail.com>
2023-10-26 09:17:44 +0000

    Modify the 'retrieve_from_database' function to return empty array if data is not found

--- database_setups.py
+++ database_setups.py
@@ -5,16 +5,22 @@ import os
 # It takes 2 option:
 #   1. The users to save in the database
 #   2. The filename, the name of the json file or we can call it the "database name"
+
+
 def save_to_database(data, database_filename):
-       # Create the 'database' folder if it doesn't exist
+    # Create the 'database' folder if it doesn't exist
     if not os.path.exists('database'):
         os.mkdir('database')
     with open("database/" + database_filename, 'w') as json_file:
         return json.dump(data, json_file, indent=4)
-    
 
-# This function retrieves or get the data from json file or database (to mimics a database)    
+
+# This function retrieves or get the data from json file or database (to mimic a database)
 def retrieve_from_database(database_filename):
-    with open("database/" + database_filename, 'r') as json_file:
-        data = json.load(json_file)
-        return data
+    # First check to get the data, retun an empty array in case it can't get the data
+    try:
+        with open("database/" + database_filename, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError:
+        return []
